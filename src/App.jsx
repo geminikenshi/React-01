657f85ece04568121ceba7520719aba2d1fa62f1
Kenshi <james85529@gmail.com>
2023-12-13 12:28:57 +0800

    refactor: seperate components by feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import { useState } from "react";
-
-import componentImage from "./assets/components.png";
-import { CORE_CONCEPTS, EXAMPLES } from "./data";
 import Header from "./components/Header/Header";
-import TabButton from "./components/TabButton";
 import CoreConcepts from "./components/CoreConcepts";
 import Examples from "./components/Examples";
 
 function App() {
-  console.log("APP component executing");
-
   return (
     <>
       <Header />
